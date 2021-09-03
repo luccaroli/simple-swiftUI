e68e76fb3e3512e33c2d7f99243a2d47eab9b178
luccaroli <luccaroli20@gmail.com>
2021-09-03 00:12:42 -0300

    Initial Commit

---
+++ simpleTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simpleTest
+//
+//  Created by Lucas Oliveira on 03/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleTest/simpleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleTestApp.swift
+//  simpleTest
+//
+//  Created by Lucas Oliveira on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct simpleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
